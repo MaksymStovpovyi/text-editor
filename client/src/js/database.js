ae9974792c5f7d0db9b985a4cb40d3dc8355a4a7
maxSt <stovpovymax@gmail.com>
2023-03-19 21:22:18 -0400

    added database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -11,11 +11,25 @@ const initdb = async () =>
       console.log('jate database created');
     },
   });
+// logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  const jateDB = await openDB("jate", 1);
+  const tx = jateDB.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log("putDB - OK!", result);
+};
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// logic for a method that gets all the content from the database
+export const getDb = async () => {
+  const jateDB = await openDB("jate", 1);
+  const tx = jateDB.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.getAll();
+  const result = await request;
+  console.log("getDB - OK!", result);
+  return result.value;
+};
 
 initdb();
